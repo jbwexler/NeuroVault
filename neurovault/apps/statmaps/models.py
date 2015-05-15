3e5f92fe259a55eff20df6a1bfc277e31cb26fef
jbwexler <jbwexler23@gmail.com>
2015-05-15 14:49:43 -0700

    testing travis

--- neurovault/apps/statmaps/models.py
+++ neurovault/apps/statmaps/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db import models
 from neurovault.apps.statmaps.storage import NiftiGzStorage, NIDMStorage
-
+ 
 from taggit.managers import TaggableManager
 from taggit.models import GenericTaggedItemBase, TagBase
 from xml import etree
